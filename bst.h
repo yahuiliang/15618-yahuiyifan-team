17f70593531f07dcf0a6400837bc3c46438b4381
Yahui Liang <harryliang52@163.com>
2021-11-10 00:17:26 -0500

    implemented the general version of BST

--- bst.h
+++ bst.h
@@ -1,47 +1,202 @@
 #ifndef BST_H
 #define BST_H
 
+#include <stdio.h>
+
 template<typename T>
 class BST {
-public:
+protected:
     struct Node {
         Node* left;
         Node* right;
         T val;
+        Node(const T& _val) {
+            left = nullptr;
+            right = nullptr;
+            val = _val;
+        }
     };
-    virtual void insert(const T& t)=0;
+public:
+    virtual bool insert(const T& t)=0;
     virtual void erase(const T& t)=0;
-    virtual void find(const T& t)=0;
+    virtual bool find(const T& t) const=0;
+    virtual size_t size()=0;
 };
 
 template<typename T>
 class CoarseGrainedBST : public BST<T> {
-    typename BST<T>::Node* root;
+    typedef typename BST<T>::Node node_t;
+    node_t* root;
+    size_t _size;
+    bool insert_helper(node_t* node, const T& elemnt);
+    bool find_helper(const node_t* node, const T& element) const;
+    void erase_helper(node_t* parent, node_t* node, const T& element);
+    void free_helper(node_t* node);
 public:
     CoarseGrainedBST();
-    void insert(const T& t);
-    void erase(const T& t);
-    void find(const T& t);
+    virtual ~CoarseGrainedBST();
+    virtual bool insert(const T& t);
+    virtual void erase(const T& t);
+    virtual bool find(const T& t) const;
+    virtual size_t size();
 };
 
 template<typename T>
-CoarseGrainedBST<T>::CoarseGrainedBST() {
-    
+CoarseGrainedBST<T>::CoarseGrainedBST(): root(nullptr), _size(0) {}
+
+template<typename T>
+CoarseGrainedBST<T>::~CoarseGrainedBST() {
+    free_helper(root);
 }
 
 template<typename T>
-void CoarseGrainedBST<T>::insert(const T& t) {
-    
+void CoarseGrainedBST<T>::free_helper(node_t* node) {
+    if (node == nullptr) {
+        return;
+    }
+    free_helper(node->left);
+    free_helper(node->right);
+    delete node;
+}
+
+template<typename T>
+bool CoarseGrainedBST<T>::insert(const T& t) {
+    if (root == nullptr) {
+        root = new node_t(t);
+        _size++;
+        return true;
+    }
+    bool inserted = insert_helper(root, t);
+    if (inserted) {
+        _size++;
+    }
+    return inserted;
+}
+
+template<typename T>
+bool CoarseGrainedBST<T>::insert_helper(node_t* node, const T& element) {
+    const T& node_val = node->val;
+    node_t* left = node->left;
+    node_t* right = node->right;
+    bool inserted = false;
+    if (element == node_val) {
+        inserted = false;
+    } else if (element < node_val) {
+        if (left == nullptr) {
+            node_t* new_node = new node_t(element);
+            node->left = new_node;
+            inserted = true;
+        } else {
+            inserted = insert_helper(left, element);
+        }
+    } else {
+        if (right == nullptr) {
+            node_t* new_node = new node_t(element);
+            node->right = new_node;
+            inserted = true;
+        } else {
+            inserted = insert_helper(right, element);
+        }
+    }
+    return inserted;
 }
 
 template<typename T>
 void CoarseGrainedBST<T>::erase(const T& t) {
-    
+    erase_helper(root, root, t);
 }
 
 template<typename T>
-void CoarseGrainedBST<T>::find(const T& t) {
-    
+void CoarseGrainedBST<T>::erase_helper(node_t* parent, node_t* node, const T& element) {
+    if (node == nullptr) {
+        return;
+    }
+    const T& val = node->val;
+    node_t* left = node->left;
+    node_t* right = node->right;
+    node_t* parent_left = parent->left;
+    node_t* parent_right = parent->right;
+    if (element == val) {
+        node_t* neighbor = node->left;
+        node_t* neighbor_parent = node;
+        node_t* neighbor_left = nullptr;
+        node_t* neighbor_right = nullptr;
+        while (neighbor != nullptr && neighbor->right != nullptr) {
+            neighbor_parent = neighbor;
+            neighbor = neighbor->right;
+        }
+        if (neighbor == nullptr) {
+            neighbor = node->right;
+            neighbor_parent = node;
+            while (neighbor != nullptr && neighbor->left != nullptr) {
+                neighbor_parent = neighbor;
+                neighbor = neighbor->left;
+            }
+        }
+        if (neighbor != nullptr) {
+            neighbor_left = neighbor->left;
+            neighbor_right = neighbor->right;
+        }
+        node_t* neighbor_parent_left = neighbor_parent->left;
+        node_t* neighnor_parent_right = neighbor_parent->right;
+        if (parent_left == node) {
+            parent->left = neighbor;
+        } else if (parent_right == node) {
+            parent->right = neighbor;
+        }
+        if (neighbor != nullptr) {
+            if (left == neighbor) {
+                left = neighbor_left;
+            }
+            neighbor->left = left;
+            if (right == neighbor) {
+                right = neighbor_right;
+            }
+            neighbor->right = right;
+        }
+        if (neighbor_parent_left == neighbor) {
+            neighbor_parent->left = neighbor_right;
+        }
+        if (neighnor_parent_right == neighbor) {
+            neighbor_parent->right = neighbor_left;
+        }
+        if (node == root) {
+            root = neighbor;
+        }
+        _size--;
+        delete node;
+    } else if (element < val) {
+        erase_helper(node, node->left, element);
+    } else {
+        erase_helper(node, node->right, element);
+    }
+}
+
+template<typename T>
+bool CoarseGrainedBST<T>::find(const T& t) const {
+    return find_helper(root, t);
+}
+
+template<typename T>
+bool CoarseGrainedBST<T>::find_helper(const node_t* node, const T& element) const {
+    if (node == nullptr) {
+        return false;
+    }
+    bool found = false;
+    const T& val = node->val;
+    if (element == val) {
+        found = true;
+    } else if (element < val) {
+        found = find_helper(node->left, element);
+    } else {
+        found = find_helper(node->right, element);
+    } 
+    return found;
+}
+
+template<typename T>
+size_t CoarseGrainedBST<T>::size() {
+    return _size;
 }
 
 template<typename T>
@@ -49,9 +204,10 @@ class FineGrainedBST : public BST<T> {
     typename BST<T>::Node* root;
 public:
     FineGrainedBST();
-    void insert(const T& t);
-    void erase(const T& t);
-    void find(const T& t);
+    virtual void insert(const T& t);
+    virtual void erase(const T& t);
+    virtual void find(const T& t);
+    virtual size_t size();
 };
 
 template<typename T>
@@ -74,9 +230,10 @@ class LockFreeBST : public BST<T> {
     typename BST<T>::Node* root;
 public:
     LockFreeBST();
-    void insert(const T& t);
-    void erase(const T& t);
-    void find(const T& t);
+    virtual void insert(const T& t);
+    virtual void erase(const T& t);
+    virtual void find(const T& t);
+    virtual size_t size();
 };
 
 template<typename T>
--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include "bst.h"
 #include <iostream>
+#include <cassert>
+#include <vector>
+#include <climits>
+#include <unordered_set>
+
+#define TEST_SIZE 1000
+#define RAND_RANGE 1000
+// #define INPUT_PRINT
 
 int main() {
-    printf("Hello World\n");
+    srand(time(NULL));
+    CoarseGrainedBST<int> bst;
+    std::vector<int> elements(TEST_SIZE);
+    size_t n = elements.size();
+    for (size_t i = 0; i < n; i++) {
+        elements[i] = rand() % RAND_RANGE;
+    }
+    #ifdef INPUT_PRINT
+    printf("inputs: ");
+    for (int test : elements) {
+        printf("%d,", test);
+    }
+    printf("\n");
+    #endif
+    std::unordered_set<int> unique_elements(elements.begin(), elements.end());
+    for (int test : elements) {
+        bst.insert(test);
+    }
+    assert(bst.size() == unique_elements.size());
+    for (int test : elements) {
+        assert(bst.find(test) == true);
+    }
+    assert(bst.find(INT_MIN) == false);
+    assert(bst.find(INT_MAX) == false);
+    for (int test : elements) {
+        bst.erase(test);
+        assert(bst.find(test) == false);
+    }
+    assert(bst.size() == 0);
+    printf("test passed\n");
     return 0;
 }
